a2830ff4a8070122b3b2715e43103d0998448bdc
Yordan <d_yugioh@abv.bg>
2021-05-28 10:27:27 +0300

    scheduler v2.0.0

--- Scheduler.Web/Startup.cs
+++ Scheduler.Web/Startup.cs
@@ -17,7 +17,6 @@ namespace Scheduler.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
@@ -26,9 +25,10 @@ namespace Scheduler.Web
                     options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SchedulerWebContext dbContext)
         {
+            dbContext.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -36,7 +36,6 @@ namespace Scheduler.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
